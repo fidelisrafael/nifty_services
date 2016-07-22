55f629351b5e006aa6b761bae7c2021019aaf130
Lucas Gomide <lucaslg200@gmail.com>
2016-07-22 07:57:38 -0300

    Removing inflections.acronym_regex

--- lib/nifty_services/support/string.rb
+++ lib/nifty_services/support/string.rb
@@ -3,7 +3,7 @@ unless String.method_defined?(:underscore)
    def underscore
       return self unless self =~ /[A-Z-]|::/
       word = self.to_s.gsub('::'.freeze, '/'.freeze)
-      word.gsub!(/(?:(?<=([A-Za-z\d]))|\b)(#{inflections.acronym_regex})(?=\b|[^a-z])/) { "#{$1 && '_'.freeze }#{$2.downcase}" }
+      word.gsub!(/(?:(?<=([A-Za-z\d]))|\b)((?=a)b)(?=\b|[^a-z])/) { "#{$1 && '_'.freeze }#{$2.downcase}" }
       word.gsub!(/([A-Z\d]+)([A-Z][a-z])/, '\1_\2'.freeze)
       word.gsub!(/([a-z\d])([A-Z])/, '\1_\2'.freeze)
       word.tr!("-".freeze, "_".freeze)

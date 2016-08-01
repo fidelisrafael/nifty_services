1bd82d8807e739c6017ab85c806fbbb7b99a9317
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2016-08-01 11:57:11 -0300

    More semantic parameters names

--- lib/nifty_services/util.rb
+++ lib/nifty_services/util.rb
@@ -1,9 +1,9 @@
 module NiftyServices
   module Util
-    def normalized_callback_name(callback_name, prefix = '_callback')
-      cb_name = callback_name.to_s.gsub(%r(\Z#{prefix}), '')
+    def normalized_callback_name(callback_name, suffix = '_callback')
+      cb_name = callback_name.to_s.gsub(%r(\Z#{suffix}), '')
 
-      [cb_name, prefix].join
+      [cb_name, suffix].join
     end
     module_function :normalized_callback_name
   end

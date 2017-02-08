a06bc0110d6d9e6c3b8188212adccb0dd48e56fa
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2017-02-08 17:32:28 -0200

    Handling better methods call to avoid useless object allocation (#37)
    Better strategy for object allocation

--- lib/nifty_services/util.rb
+++ lib/nifty_services/util.rb
@@ -1,9 +1,11 @@
 module NiftyServices
   module Util
     def normalized_callback_name(callback_name, suffix = '_callback')
-      cb_name = callback_name.to_s.gsub(%r(\Z#{suffix}), '')
+      cb_name = callback_name.to_s.end_with?(suffix) ?
+                  callback_name.to_s.sub(/#{suffix}\Z/, '') :
+                  callback_name.to_s
 
-      [cb_name, suffix].join
+      cb_name << suffix
     end
     module_function :normalized_callback_name
   end
